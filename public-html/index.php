4d3ad6ea2ed09ec5a719c0cb06aac11e574dd600
Nancy <nancyrosev@outlook.com>
2021-12-15 01:47:44 +0200

    initial commit

---
+++ public-html/index.php
@@ -0,0 +1,17 @@
+<?php
+$website = 'http://www.zavrel.net'
+?>
+
+<!DOCTYPE html>
+<head>
+    <title>Hello World!</title>
+</head>
+
+<body>
+    <h1>Hello World!</h1>
+   
+ <a href="<?php echo $website; ?>"><?php echo $wbsite; ?></a>
+</body>
+
+    
+
